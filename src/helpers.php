2fa19a9948f9813124df4ee41f448b18aefd87c3
ratno putro sulistiyono <ratno@knoqdown.com>
2018-10-11 15:54:45 +0700

    cek dahulu akses ke server git, klo gagal munculin id_rsa.pub untuk di simpan di server git, klo sukses baru composer

--- src/helpers.php
+++ src/helpers.php
@@ -13,9 +13,42 @@ function composer_create_repositories($paket,$composer_filepath,$vcs="")
 
     if($create_new) {
         if($vcs) {
-            passthru("composer config repositories.{$paket_flat} vcs git@{$vcs}:{$paket}.git");
+            $command = "composer config repositories.{$paket_flat} vcs git@{$vcs}:{$paket}.git";
         } else {
-            passthru("composer config repositories.{$paket_flat} path $paket");
+            $command = "composer config repositories.{$paket_flat} path $paket";
         }
+        
+        passthru($command);
     }
+}
+
+function run_proc($command)
+{
+    $env = NULL;
+    $options = array('bypass_shell' => true);
+    $cwd = NULL;
+    $descriptorspec = [
+        0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+        1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+        2 => array("pipe", "w")  // stderr is a file to write to
+    ];
+
+    $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env, $options);
+
+    $return = [];
+    if (is_resource($process)) {
+        while ($f = fgets($pipes[1])) {
+            $return['output'][] = trim($f);
+        }
+        fclose($pipes[1]);
+        while ($f = fgets($pipes[2])) {
+            $return['error'][] = trim($f);
+        }
+        fclose($pipes[2]);
+
+        // It is important that you close any pipes before calling
+        // proc_close in order to avoid a deadlock
+        $return['value'] = proc_close($process);   
+    }
+    return $return;
 }
