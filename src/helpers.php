5efd3074fa4d64a6a1e0d98a4bec94dd226c1723
ratno putro sulistiyono <ratno@knoqdown.com>
2018-10-11 17:09:28 +0700

    masih problem local path package

--- src/helpers.php
+++ src/helpers.php
@@ -1,25 +1,50 @@
 <?php
 function composer_create_repositories($paket,$composer_filepath,$vcs="")
 {
-    $paket_flat = str_replace("/","_",$paket);
 
+    $command = "";
+    $return = $paket;
+
+    if($vcs) {
+        if(check_create_repositories($paket,$composer_filepath)) {
+            $paket_flat = get_paket_flat($paket);
+            $command = "composer config repositories.{$paket_flat} vcs git@{$vcs}:{$paket}.git";
+        }
+    } else {
+        // $paket yg diinput disini adalah path menuju composer vendor
+        $composer_vendor = json_decode(file_get_contents($paket),true);
+        $paket_name = $composer_vendor['name'];
+        $paket_flat = get_paket_flat($paket_name);
+        $paket_path = str_replace("composer.json","",$paket);
+        if(check_create_repositories($paket_name,$composer_filepath)) {
+            $command = "composer config repositories.{$paket_flat} path $paket_path";
+            $return = $paket_name;
+        }
+    }
+    
+    if($command) {
+        passthru($command);
+    }
+
+    return $return;
+}
+
+function check_create_repositories($paket_name,$composer_filepath)
+{
     $composer = json_decode(file_get_contents($composer_filepath),true);
     $create_new = true;
     if(array_key_exists("repositories",$composer)) {
-        if(array_key_exists($paket_flat,$composer["repositories"])) {
+        if(array_key_exists(get_paket_flat($paket_name),$composer["repositories"])) {
             $create_new = false;
         }
     }
 
-    if($create_new) {
-        if($vcs) {
-            $command = "composer config repositories.{$paket_flat} vcs git@{$vcs}:{$paket}.git";
-        } else {
-            $command = "composer config repositories.{$paket_flat} path $paket";
-        }
-        
-        passthru($command);
-    }
+    return $create_new;
+}
+
+function get_paket_flat($paket_name)
+{
+    return str_replace("/","_",$paket_name);
 }
 
 function run_proc($command)
